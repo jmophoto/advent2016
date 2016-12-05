95ebee858d8ab9526042f1368eac08bd7db1a3c7
jmo <jason.olson@mx.com>
2016-12-05 08:57:45 -0700

    day4b.js names

---
+++ day4b.js
@@ -0,0 +1,63 @@
+const inputs = require('./day4inputs.js');
+
+const names = [];
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+const decodeRoom = (input) => {
+  const string = input.replace(/-/gi, '');
+  const name = string.slice(0, -10);
+  const sector = parseInt(string.slice(-10, -7));
+  const checksum = string.slice(-7).replace('[', '').replace(']', '');
+
+  const letters = name.split('').filter((letter, index) => name.indexOf(letter) === index).sort((a, b) => {
+    const regexA = new RegExp(a, 'gi');
+    const regexB = new RegExp(b, 'gi');
+
+    if (name.match(regexA).length > name.match(regexB).length) {
+      return -1;
+    }
+
+    if (name.match(regexA).length < name.match(regexB).length) {
+      return 1;
+    }
+
+    if (a > b) {
+      return 1;
+    }
+
+    if (a < b) {
+      return -1;
+    }
+
+    return 0;
+  }).splice(0, 5).join('');
+
+  const valid = letters === checksum;
+
+  return { sector, valid };
+}
+
+decodeName = (sector, input) => {
+  return input.slice(0, -11).split('').map((letter, index) => {
+    if (letter === '-') {
+      return ' ';
+    }
+
+    const advancedIndex = alphabet.indexOf(letter) + (sector % alphabet.length);
+    const characterIndex = advancedIndex > alphabet.length - 1 ? advancedIndex - alphabet.length : advancedIndex;
+
+    return alphabet.charAt(characterIndex);
+
+  }).join('');
+}
+
+inputs.forEach(input => {
+  const { sector, valid } = decodeRoom(input);
+
+  if (valid) {
+    console.log(decodeName(sector, input));
+    names.push(decodeName(sector, input));
+  }
+})
+
+console.log(`The sector total is ${sectorTotal}.`);
