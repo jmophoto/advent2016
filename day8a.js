3ac7fb8b23b1a703c23029344d2aab91a06c320e
jmo <jason.olson@mx.com>
2016-12-09 08:28:05 -0700

    day8a

---
+++ day8a.js
@@ -0,0 +1,72 @@
+const inputs = require('./day8inputs.js');
+
+const screen = [];
+
+for (let i = 0; i < 6; i++) {
+  screen [i] = [];
+  for (let j = 0; j < 50; j++) {
+    screen[i][j] = 0;
+  }
+}
+
+const createRect = (x, y) => {
+  for (let i = 0; i < y; i++) {
+    for (let j = 0; j < x; j++) {
+      screen[i][j] = 1;
+    }
+  }
+};
+
+const test = [
+  'rect 3x2',
+  'rotate column x=1 by 1',
+  'rotate row y=0 by 4',
+  'rotate column x=1 by 1'
+];
+
+inputs.forEach(input => {
+  const pieces = input.split(' ');
+
+  if (pieces[0].includes('rect')) {
+    const coords = pieces[1].split('x');
+    createRect(coords[0], coords[1]);
+  }
+
+  if (pieces[1].includes('column')) {
+    const column = pieces[2].split('=')[1];
+    const count = pieces[4];
+    const values = [];
+
+    for (let i = 0; i < screen.length; i++) {
+      values.push(screen[i][column]);
+    }
+
+    for (let i = 0; i < count; i++) {
+      values.unshift(values.pop());
+    }
+
+    for (let i = 0; i < screen.length; i++) {
+      screen[i][column] = values[i];
+    }
+  }
+
+  if (pieces[1].includes('row')) {
+    const row = pieces[2].split('=')[1];
+    const count = pieces[4];
+
+    for (let i = 0; i < count; i++) {
+      screen[row].unshift(screen[row].pop());
+    }
+  }
+});
+
+let total = 0;
+
+for (let i = 0; i < screen.length; i++) {
+  for (var j = 0; j < screen[i].length; j++) {
+    console.log(i, j, screen[i][j], total);
+    total = total + screen[i][j];
+  }
+}
+
+console.log(`The total count of lit pixels is ${total}.`);
