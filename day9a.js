d1ecfcfae0d57df5d266d8c122c8802f01e46df8
jmo <jason.olson@mx.com>
2016-12-12 19:10:04 -0700

    day 9a

---
+++ day9a.js
@@ -0,0 +1,44 @@
+const inputs = require('./day9inputs.js');
+
+const letters = inputs.split('');
+const decoded = [];
+let letter = '';
+
+do {
+  letter = letters.shift();
+  if (letter === '(') {
+    let characterCount = [];
+    let repeatCount = [];
+
+    letter = letters.shift();
+
+    do {
+      characterCount.push(letter);
+      letter = letters.shift();
+    } while (letter !== 'x');
+
+    letter = letters.shift();
+
+    do {
+      repeatCount.push(letter);
+      letter = letters.shift();
+    } while (letter !== ')');
+
+    characterCount = characterCount.join('');
+    repeatCount = repeatCount.join('');
+
+    repeatedLetters = [];
+
+    for (var i = 0; i < (characterCount); i++) {
+      repeatedLetters.push(letters.shift());
+    }
+
+    decoded.push(repeatedLetters.join('').repeat(repeatCount));
+
+  } else {
+    decoded.push(letter);
+  }
+} while (letters.length);
+
+
+console.log(decoded.join('').length);
